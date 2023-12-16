808d0cbabe16b69353b68a19d4a00492ad511aef
Helixjoe <joeljohn5112@gmail.com>
2023-12-16 09:37:46 +0530

    slider fixed and info buttons added

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,10 +1,10 @@
 
 function EventCard(props) {
     const imgLocation = `url("./events/` + props.loc + `")`;
-    return (<div className="eventCard" style={{
+    return (<div><div className="eventCard" style={{
         backgroundImage: imgLocation,
         opacity: 0.9
-    }}></div >);
+    }}></div ><div className="eventInfo"><p>More Info</p></div></div >);
 }
 
 export default EventCard;
